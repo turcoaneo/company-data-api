613d4dc59f574c43f8b95e5af0134d396d2e28aa
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2026-05-25 13:37:04 +0300

    AWS NAT 2.

--- infra/terraform/ecs.tf
+++ infra/terraform/ecs.tf
@@ -94,6 +94,27 @@ resource "aws_ecs_task_definition" "app_task_def" {
   ])
 }
 
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = module.vpc.public_subnets[0]
+
+  tags = {
+    Name = "company-nat-gateway"
+  }
+}
+
+resource "aws_route" "private_nat_routes" {
+  for_each = toset(module.vpc.private_route_table_ids)
+
+  route_table_id         = each.value
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
 resource "aws_ecs_service" "app_service" {
   name            = var.ecs_service_name
   cluster         = aws_ecs_cluster.app_cluster.id
@@ -112,10 +133,11 @@ resource "aws_ecs_service" "app_service" {
     container_name   = "app"
     container_port   = var.app_port
   }
-}
 
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
+  depends_on = [
+    aws_nat_gateway.nat,
+    aws_route.private_nat_routes
+  ]
 }
 
 resource "aws_nat_gateway" "nat" {
