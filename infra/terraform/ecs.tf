ee8f822acc1b5802e61178996ed0a0216ac0fb9b
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2026-05-25 13:46:37 +0300

    AWS NAT again.

--- infra/terraform/ecs.tf
+++ infra/terraform/ecs.tf
@@ -139,21 +139,3 @@ resource "aws_ecs_service" "app_service" {
     aws_route.private_nat_routes
   ]
 }
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = module.vpc.public_subnets[0]
-
-  tags = {
-    Name = "company-nat-gateway"
-  }
-}
-
-resource "aws_route" "private_nat_routes" {
-  for_each = toset(module.vpc.private_route_table_ids)
-
-  route_table_id         = each.value
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
-}
-
